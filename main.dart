86ffd50db9cea5b9d0807e20635bb5bd799ca472
Shashwat Pandey <cool17shashwat@gmail.com>
2020-04-08 21:29:08 +0530

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'login.dart';
+import 'signup.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Spotify Clone',
+      theme: ThemeData.dark().copyWith(
+        //primaryColor: Color(0xff388e3c),
+        //accentColor: Color(0xff388e3c),
+        //buttonColor: Color(0xff388e3c),
+        //scaffoldBackgroundColor: Color(0xff00000e),
+
+      ),
+      debugShowCheckedModeBanner: false,
+      home: LoginPage(),
+      routes: {
+        '/login': (context) => LoginPage(),
+        '/signup': (context) => RegisterPage(),
+      },
+    );
+  }
+}
